fd20b1fa3dff4a0436e3a39a1d987a02f59096bb
mercurychu <rudals2448@naver.com>
2020-05-21 13:26:23 +0900

    no message

---
+++ java0521/src/java0521/IntArray54321.java
@@ -0,0 +1,13 @@
+package java0521;
+
+public class IntArray54321 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a = {5,4,3,2,1};
+		
+		for (int i = 0; i < a.length; i++) 
+			System.out.println("a[" + i + "] = " + a[i]);
+	}
+
+}
